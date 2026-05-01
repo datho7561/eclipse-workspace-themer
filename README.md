60660f3f45bca9227fa3a24c9779cae79dabffd4
David Thompson <davthomp@redhat.com>
2026-05-01 11:53:34 -0400

    Initial commit
    Signed-off-by: David Thompson <davthomp@redhat.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# eclipse-workspace-themer
+
+Set the theme for a given workspace for Eclipse IDE. Focuses on the Java IDE specifically. Currently supplies one theme: gruvbox light. Uses [bubbletea](https://github.com/charmbracelet/bubbletea) and [lipgloss](https://github.com/charmbracelet/lipgloss).
+
+## Building
+
+1. clone
+2. `go build`
+3. copy `eclipse-workspace-themer` onto `$PATH`
+
+## Usage
+
+1. `cd` into your Eclipse workspace
+2. Run `eclipse-workspace-themer`, select theme with arrow keys, then press enter.
---
+++ main.go
@@ -0,0 +1,266 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	tea "charm.land/bubbletea/v2"
+	lipgloss "charm.land/lipgloss/v2"
+)
+
+var cursor_style = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Green)
+
+var selected_style = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.White).
+	Background(lipgloss.Cyan)
+
+var theme_map = map[string]theme{
+	"gruvbox-light": {
+		foreground:              theme_color{r: 60, g: 56, b: 54},
+		background:              theme_color{r: 251, g: 241, b: 199},
+		selection_foreground:    theme_color{r: 251, g: 241, b: 199},
+		selection_background:    theme_color{r: 146, g: 131, b: 116},
+		current_line_background: theme_color{r: 235, g: 219, b: 178},
+		line_number_color:       theme_color{r: 168, g: 153, b: 132},
+		comment_color:           theme_color{r: 146, g: 131, b: 116},
+		field_color:             theme_color{r: 7, g: 102, b: 120},
+		keyword_color:           theme_color{r: 143, g: 62, b: 113},
+		string_color:            theme_color{r: 66, g: 123, b: 87},
+		local_variable_color:    theme_color{r: 121, g: 116, b: 14},
+		method_color:            theme_color{r: 175, g: 58, b: 2},
+	},
+}
+
+var eclipse_ui_to_modify = map[string]bool{
+	"AbstractTextEditor.Color.Background":                        true,
+	"AbstractTextEditor.Color.Background.SystemDefault":          true,
+	"AbstractTextEditor.Color.Foreground":                        true,
+	"AbstractTextEditor.Color.Foreground.SystemDefault":          true,
+	"AbstractTextEditor.Color.SelectionBackground":               true,
+	"AbstractTextEditor.Color.SelectionBackground.SystemDefault": true,
+	"AbstractTextEditor.Color.SelectionForeground":               true,
+	"AbstractTextEditor.Color.SelectionForeground.SystemDefault": true,
+	"currentLineColor":            true,
+	"eclipse.preferences.version": true,
+	"lineNumberColor":             true,
+	"printMarginColor":            true,
+}
+
+var eclipse_jdt_ui_to_modify = map[string]bool{
+	"java_comment_task_tag":                             true,
+	"java_doc_default":                                  true,
+	"java_doc_keyword":                                  true,
+	"java_doc_link":                                     true,
+	"java_doc_tag":                                      true,
+	"java_keyword":                                      true,
+	"java_keyword_return":                               true,
+	"java_multi_line_comment":                           true,
+	"java_single_line_comment":                          true,
+	"java_string":                                       true,
+	"semanticHighlighting.annotation.color":             true,
+	"semanticHighlighting.field.color":                  true,
+	"semanticHighlighting.localVariable.color":          true,
+	"semanticHighlighting.restrictedKeywords.color":     true,
+	"semanticHighlighting.staticField.color":            true,
+	"semanticHighlighting.staticFinalField.color":       true,
+	"semanticHighlighting.method.color":                 true,
+	"semanticHighlighting.method.enabled":               true,
+	"semanticHighlighting.methodDeclarationName.color":  true,
+	"semanticHighlighting.staticMethodInvocation.color": true,
+	"sourceHoverBackgroundColor":                        true,
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func initialModel() model {
+	keys := make([]string, 0, len(theme_map))
+	for k := range theme_map {
+		keys = append(keys, k)
+	}
+	return model{
+		choices: keys,
+		cursor:  0,
+	}
+}
+
+type model struct {
+	choices []string
+	cursor  int
+}
+
+type theme struct {
+	foreground              theme_color
+	background              theme_color
+	selection_foreground    theme_color
+	selection_background    theme_color
+	current_line_background theme_color
+	line_number_color       theme_color
+	keyword_color           theme_color
+	comment_color           theme_color
+	string_color            theme_color
+	field_color             theme_color
+	local_variable_color    theme_color
+	method_color            theme_color
+}
+
+type theme_color struct {
+	r int
+	g int
+	b int
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyPressMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key selects the theme
+		case "enter":
+			setTheme(theme_map[m.choices[m.cursor]])
+			return m, tea.Quit
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func setTheme(t theme) {
+	wd, err := os.Getwd()
+	check(err)
+
+	// first settings file; o.e.ui
+	path1 := filepath.Join(wd, ".metadata", ".plugins", "org.eclipse.core.runtime", ".settings", "org.eclipse.ui.editors.prefs")
+	dat, err := os.ReadFile(path1)
+	check(err)
+	entries := strings.Split(string(dat), "\n")
+	new_entries := []string{}
+	for i := range entries {
+		parsed_line := strings.Split(entries[i], "=")
+		if !eclipse_ui_to_modify[parsed_line[0]] {
+			new_entries = append(new_entries, entries[i])
+		}
+	}
+	new_entries = append(new_entries, fmt.Sprintf("AbstractTextEditor.Color.Background=%s", t.background))
+	new_entries = append(new_entries, "AbstractTextEditor.Color.Background.SystemDefault=false")
+	new_entries = append(new_entries, fmt.Sprintf("AbstractTextEditor.Color.Foreground=%s", t.foreground))
+	new_entries = append(new_entries, "AbstractTextEditor.Color.Foreground.SystemDefault=false")
+	new_entries = append(new_entries, fmt.Sprintf("AbstractTextEditor.Color.SelectionBackground=%s", t.selection_background))
+	new_entries = append(new_entries, "AbstractTextEditor.Color.SelectionBackground.SystemDefault=false")
+	new_entries = append(new_entries, fmt.Sprintf("AbstractTextEditor.Color.SelectionForeground=%s", t.selection_foreground))
+	new_entries = append(new_entries, "AbstractTextEditor.Color.SelectionForeground.SystemDefault=false")
+	new_entries = append(new_entries, fmt.Sprintf("currentLineColor=%s", t.current_line_background))
+	new_entries = append(new_entries, fmt.Sprintf("lineNumberColor=%s", t.line_number_color))
+	new_entries = append(new_entries, fmt.Sprintf("printMarginColor=%s", t.background))
+	err = os.WriteFile(path1, []byte(strings.Join(new_entries, "\n")), 0644)
+	check(err)
+
+	// second settings file; o.e.j.ui
+	path2 := filepath.Join(wd, ".metadata", ".plugins", "org.eclipse.core.runtime", ".settings", "org.eclipse.jdt.ui.prefs")
+	dat, err = os.ReadFile(path2)
+	check(err)
+	entries = strings.Split(string(dat), "\n")
+	new_entries = []string{}
+	for i := range entries {
+		parsed_line := strings.Split(entries[i], "=")
+		if !eclipse_jdt_ui_to_modify[parsed_line[0]] {
+			new_entries = append(new_entries, entries[i])
+		}
+	}
+	new_entries = append(new_entries, fmt.Sprintf("java_comment_task_tag=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_doc_default=%s", t.comment_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_doc_keyword=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_doc_link=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_doc_tag=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_keyword=%s", t.keyword_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_keyword_return=%s", t.keyword_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_multi_line_comment=%s", t.comment_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_single_line_comment=%s", t.comment_color))
+	new_entries = append(new_entries, fmt.Sprintf("java_string=%s", t.string_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.annotation.color=%s", t.comment_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.field.color=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.localVariable.color=%s", t.local_variable_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.restrictedKeywords.color=%s", t.keyword_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.staticField.color=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.staticFinalField.color=%s", t.field_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.method.color=%s", t.method_color))
+	new_entries = append(new_entries, "semanticHighlighting.method.enabled=true")
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.methodDeclarationName.color=%s", t.method_color))
+	new_entries = append(new_entries, fmt.Sprintf("semanticHighlighting.staticMethodInvocation.color=%s", t.method_color))
+	new_entries = append(new_entries, fmt.Sprintf("sourceHoverBackgroundColor=%s", t.background))
+	err = os.WriteFile(path2, []byte(strings.Join(new_entries, "\n")), 0644)
+	check(err)
+}
+
+func (c theme_color) String() string {
+	return fmt.Sprintf("%d,%d,%d", c.r, c.g, c.b)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func (m model) View() tea.View {
+	// The header
+	s := "Which theme should be used?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		fancified_choice := choice
+		if m.cursor == i {
+			cursor = ">" // cursor!
+			fancified_choice = selected_style.Render(choice)
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s %s\n", cursor_style.Render(cursor), fancified_choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return tea.NewView(s)
+}
